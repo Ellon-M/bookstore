c9b5ff1711fd1b22add6d6a1d81a2f19a48f435d
ellon <mordecaimwema11@gmail.com>
2023-01-20 17:45:00 +0300

    add links to each page on the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Books</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+      </Header>
     </div>
   );
 }
