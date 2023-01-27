c2c1bf7609e821a3e6e53a26f9ae5322cb1ef076
ellon <mordecaimwema11@gmail.com>
2023-01-27 20:57:59 +0300

    add profile nav icon, add progress section, adjust books list border to match design

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import {
   Link, BrowserRouter, Routes, Route,
 } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import Booklist from './components/Booklist';
 import Categories from './components/Categories';
 
 const App = () => (
-  <div className="max-w-7xl mx-auto my-5 bg-white font-inter">
+  <div className="font-inter">
     <BrowserRouter>
-      <nav className="flex items-center gap-5 shadow-md p-5">
-        <h1 className="text-[#0290ff] font-bold text-4xl">Bookstore CMS</h1>
-        <Link to="/" className="text-gray-500 text-lg font-semibold">Books</Link>
-        <Link to="/categories" className="text-gray-500 text-lg font-semibold">Categories</Link>
+      <nav className="shadow-md">
+        <div className="max-w-7xl mx-auto p-5 flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <h1 className="text-[#0290ff] font-bold text-4xl">Bookstore CMS</h1>
+            <Link to="/" className="text-gray-500 text-lg font-semibold">Books</Link>
+            <Link to="/categories" className="text-gray-500 text-lg font-semibold">Categories</Link>
+          </div>
+
+          <div className="w-10 h-10 flex items-center justify-center border rounded-full cursor-pointer">
+            <FaUser className="text-[#0290ff]" />
+          </div>
+        </div>
       </nav>
 
       <div className="p-5">
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -29,13 +29,13 @@ const AddBook = () => {
   };
 
   return (
-    <div className="mb-2">
+    <div className="mb-2 max-w-7xl mx-auto">
       <h3 className="text-gray-500 font-bold text-2xl">Add New Book</h3>
 
-      <form onSubmit={handleSubmit}>
-        <input className="border-b border-[#a0a0a0] outline-none mr-2 p-2" placeholder="Title" type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <form onSubmit={handleSubmit} className="flex justify-between items-center">
+        <input className="border border-[#a0a0a0] outline-none mr-2 p-2 w-[450px]" placeholder="Title" type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
 
-        <input className="border-b border-[#a0a0a0] outline-none mr-2 p-2" placeholder="Author" type="text" name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input className="border border-[#a0a0a0] outline-none mr-2 p-2 w-[450px]" placeholder="Author" type="text" name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
 
         <select className="py-3 px-5 outline-none cursor-pointer bg-white border rounded-md border-[#a0a0a0]" name="category" onChange={(e) => setCategory(e.target.value)}>
           {categories && categories.map((category) => (
@@ -43,7 +43,7 @@ const AddBook = () => {
           ))}
         </select>
 
-        <button type="submit" className="bg-[#0290ff] ml-2 py-2 px-5 text-white font-semibold">Add Book</button>
+        <button type="submit" className="bg-[#0290ff] ml-2 py-2 px-10 text-white font-semibold">Add Book</button>
       </form>
     </div>
   );
